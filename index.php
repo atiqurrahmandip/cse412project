dc42e7f0cd096d0ab288f50cd56c29b7dc86bfde
“atiqurrahmandip” <2018-3-60-001@std.ewubd.edu>
2023-12-26 17:47:20 +0600

    working with home page index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+
+
+<!DOCTYPE html>
+<html  lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>home page</title>
+
+        <link rel="stylesheet" href="style.css"> 
+        <link rel="stylesheet" href="componentStyle.css">
+        <script src="index.js"></script>
+
+
+
+
+
+    </head>
+    <body>
+
+        
+        
+        <?php include('components/header.php'); ?>
+
+
+       
+
+
+        
+        <div  id="banner" class=" container-fluid bg-primary py-5 px-0" >
+            <div class="row mx-0 align-items-center justify-content-end">
+                <div class="col-lg-6 px-md-5 text-center">
+                    <h1 id="banner-title" class="display-2 text-uppercase mb-3">Stationary Hub</h1>
+                    <p  id="banner-info" class="text-dark mb-4">Discover from out quality pens, notebooks, books and more. Your go-to source for creative tools and organizational essentials.Elevate your ideas with our curated collection of stylish stationary essentials for every creative mind.</p>
+                    <a id="explore-btn" class="btn  mt-1 py-3 px-5">Explore Now</a>
+                </div>
+            </div>
+        </div>
