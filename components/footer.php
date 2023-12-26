e8eac94ce4a6e76ace143da6385270feee92a5d6
“atiqurrahmandip” <2018-3-60-001@std.ewubd.edu>
2023-12-26 18:26:36 +0600

    working with components

---
+++ components/footer.php
@@ -0,0 +1,82 @@
+<footer  class="mt-1">
+    <div class="container-fluid">
+        <div class="row  text-white  pl-5  pr-5">
+
+            <div class=" footer_text col-lg-3  col-md-6  col-sm-12  p-5  pb-0 text-center">
+                <div class="title  pb-2">
+                    <h4 class="pl-3  ff-secondary  footer-title">Company</h4>
+                </div>
+                <div  style="font-size:16px; font-family: montserrat;">
+                    <a  class="footer-link">Home</a>
+                    <a  class="footer-link">About us</a>
+                    <a  class="footer-link" >Contact us</a>
+                    <a  class="footer-link">Get start</a> 
+                </div>
+            </div>
+            <div class=" footer_text col-lg-3  col-md-6  col-sm-12  p-5  pb-0 text-center">
+                <div class="title  pb-2">
+                    <h4 class="pl-3  ff-secondary  footer-title">Services</h4>
+                </div>
+                <div  style="font-size:16px; font-family: montserrat;">
+                    <a  class="footer-link">Books</a>
+                    <a  class="footer-link">Paper instruments</a>
+                    <a  class="footer-link" >Office supplies</a>
+                    <a  class="footer-link">Other</a> 
+                </div>
+                </div>
+                    <div class="footer_text col-lg-3  col-md-6  col-sm-12  p-5  pb-0  text-center">
+                        <div class="title  pb-2">
+                            <h4 class="pl-3  ff-secondary  footer-title">Accounts</h4>
+                        </div>
+                        <div  style="font-size:16px; font-family: montserrat;">
+                            <a  class="footer-link">Profile</a>
+                            <a  class="footer-link">My account</a>
+                            <a  class="footer-link" >Purchase</a>
+                            <a  class="footer-link">Preference</a> 
+                        </div>
+                    </div>
+                    <div class=" footer_text col-lg-3  col-md-6 col-sm-12  p-5  text-center">
+                        <div class="title text-left  pb-2">
+                        <h4 class="pl-3  ff-secondary  footer-title">Contact</h4>
+                    </div>
+                    <div   style="font-size:16px; font-family: montserrat;">
+                        <div ><i class="fa-sharp fa-solid fa-house"></i><p class="d-inline">  Aftebnagar,Dhaka</p></div>
+                        <div><i class="fa-sharp fa-solid fa-envelope"></i><p class="d-inline">  myshop@gmail.com</p></div>            
+                        <div><i class="fa-sharp fa-solid fa-phone"></i><p class="d-inline">  +880765643343</p></div>           
+                       <div><i class="fa-sharp fa-solid fa-paper-plane"></i><p class="d-inline"> @mystationaryshop</p></div>
+                    </div>
+                </div>
+                </div>
+                <div class="row  text-white   link-icon">
+                    <div class="footer_icon col-sm-12  col-md-12 col-lg-12  text-center  pb-3">
+                        <a class="btn text-white btn-floating m-1"style="background-color: #3b5998;  border-radius: 50%;" href="#!">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="btn text-white btn-floating m-1" style="background-color: #55acee;  border-radius: 50%;"href="#!" >
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a  class="btn text-white btn-floating m-1" style="background-color: #dd4b39; border-radius: 50%;"href="#!">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a  class="btn text-white btn-floating m-1" style="background-color: #ac2bac;  border-radius: 50%;"href="#!">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a  class="btn text-white btn-floating m-1" style="background-color: #0082ca; border-radius: 50%;"href="#!">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a  class="btn text-white btn-floating m-1" style="background-color: #333333; border-radius: 50%;" href="#!">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col-lg-12  col-md-12  col-sm-12 text-muted  text-center  pt-3">
+                        <div class="cpy_right">
+                            <p> © Copyright 2023 Company Name. All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
