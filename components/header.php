19127b192ab08ec6dff70dfa10d986f3e7c58332
“atiqurrahmandip” <2018-3-60-001@std.ewubd.edu>
2023-12-26 18:29:30 +0600

    component header added

---
+++ components/header.php
@@ -0,0 +1,39 @@
+<header class="header">
+
+    <nav  id="Navbar" class="   navbar navbar-expand-lg bg-white sticky-top navbar-light p-3 shadow-sm">
+            <div class="container-fluid">
+                <a id="logo" class="navbar-brand ff-secondary" href="#"><i class="fa-solid fa-shop me-2 "></i> <strong>ewuSS</strong></a>
+        
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto mr-2">
+                        <li class="nav-item">
+                            <a class="nav-link mx-2 active" aria-current="page"  href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2">About</a>
+                        </li>
+                        <li class="nav-item">                   
+                            <a class="nav-link mx-2">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-2">Contact</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                             Join us
+                            </a>
+                            <ul class="dropdown-menu rounded-0 m-0" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item"  href="login.php">Login</a>
+                                <a class="dropdown-item"  href="signup.php">Sign up</a>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+ </header>
